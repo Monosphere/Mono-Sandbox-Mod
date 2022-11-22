bc7a36bf51aed2464077e38fa24c3dcf23d4e977
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-11-21 22:28:27 -0330

    cee eye

--- Plugin.cs
+++ Plugin.cs
@@ -22,6 +22,7 @@ namespace MonoSandbox
     [ModdedGamemode]
     [Description("HauntedModMenu")]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInDependency("tonimacaroni.computerinterface", "1.5.3")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
